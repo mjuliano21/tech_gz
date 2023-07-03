adaedf593fca3e3a1e12c501a8cab9cb61260c41
Maicon Juliano Fritsch <mjuliano21@gmail.com>
2023-07-03 17:50:27 -0300

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,8 @@ const cors = require('./app/middlewares/cors');
 const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.json());
@@ -13,4 +15,4 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(5000, () => console.log('|Server Started at http://localhost:5000'));
+app.listen(port, () => console.log('|Server Started at http://localhost:5000'));
